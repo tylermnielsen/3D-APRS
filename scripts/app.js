762f593d3d3e1d087a7a3252c4e961c501665769
Tyler Nielsen <tylernielsenn@gmail.com>
2025-08-13 10:45:51 -0600

    starting bits and setting up untracked token storage

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+
+// setup 
+import { CESIUM_TOKEN, APRS_TOKEN } from "./tokens.js"
+
+const target = document.getElementById("cesiumView"); 
+
+// start interval 
+setInterval(update, 1000); 
+
+// updater 
+function update() {
+  
+}
+
